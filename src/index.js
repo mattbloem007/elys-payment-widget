160b8d16cc42d02c7d5b76989801c8f10ee44d82
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2021-09-15 15:14:46 +0200

    yarn installed babel-polyfill

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import 'regenerator-runtime/runtime'
+import "babel-polyfill"
 
 // Find all widget divs
 const WidgetDivs = document.querySelectorAll('.elysPaymentGateway')
