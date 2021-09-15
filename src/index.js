78f5d6358b563868ace4a72ee61832039726e04a
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2021-09-15 14:36:29 +0200

    Added runtime into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'regenerator-runtime/runtime'
 
 // Find all widget divs
 const WidgetDivs = document.querySelectorAll('.elysPaymentGateway')
