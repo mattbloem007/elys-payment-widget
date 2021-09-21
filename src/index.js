55031a660d153cd72edcda2ca752a231dd5490ba
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2021-09-21 15:20:13 +0200

    Added babel new

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import 'regenerator-runtime/runtime'
 import "babel-polyfill"
+import "core-js/stable";
+
 
 // Find all widget divs
 const WidgetDivs = document.querySelectorAll('.elysPaymentGateway')
