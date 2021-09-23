66a5bf9c6dc4911d8bdd1cfc47628135a9c8325f
Matthew Gabriel Jason Bloemetje <mattb007@gmail.com>
2021-09-23 12:06:34 +0200

    new optimised build

---
+++ payment_widget/precache-manifest.fe39bf7f561c6bff08118ed62dcc3975.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd17da808ad6043fa50a4cf9e380f4db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "92a3a806c750e3665a8d",
+    "url": "/static/css/widget.css"
+  },
+  {
+    "revision": "92a3a806c750e3665a8d",
+    "url": "/static/js/widget.js"
+  },
+  {
+    "revision": "7d9a1e97b63fb04c3f7226116d9275de",
+    "url": "/static/js/widget.js.LICENSE.txt"
+  }
+]);
